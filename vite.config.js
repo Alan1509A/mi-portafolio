73e1a5f6954aea271f7cadf2c82e69feba433ed5
Alan1509A <alaneduardo5788@gmail.com>
2026-06-19 23:31:29 -0600

    Convertir el portafolio a PWA instalable

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true // Esto permite probar la PWA mientras programamos
+      },
+      manifest: {
+        name: 'Portafolio Alan Eduardo',
+        short_name: 'EstudianteCS',
+        description: 'Portafolio, apuntes y blog de Ciencias de la Informática',
+        theme_color: '#030712', // El color oscuro de tu fondo
+        background_color: '#030712',
+        display: 'standalone', // "standalone" esconde la barra del navegador para que parezca app nativa
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
